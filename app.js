980be83d09cc568e77f4d133aad0da3503a09caf
edpuzino <edpuzino@yahoo.com>
2018-06-07 17:45:20 -0700

    All of the math working, just need to make a list

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 'use strict';
 
 /*array of objects*/
-Sales.all = [];
-Sales.justViewed = [];
+var sales = [];
+var justViewed = [];
+var totalClicks = 0;
+var first = document.getElementById('left');
+var second = document.getElementById('center');
+var third = document.getElementById('right');
 
 //constructor funtion to generate the information for each picture
 function Sales(name, filepath) {
@@ -10,8 +14,7 @@ function Sales(name, filepath) {
     this.filepath = filepath;
     this.clicks = 0;
     this.displays = 0;
-    Sales.all.push(this);
-    /*console.log(this);*/
+    sales.push(this);
 }
 
 
@@ -40,48 +43,86 @@ function populate() {
 }
 
 
+function getRandom() {
+    return Math.floor(Math.random() * sales.length);
+}
+
 //function to randomly pic three pictures out of the total, do not have the picture show more than once on the page and do not show any of the pictures from the previous page
-for(var i = 0; i < Sales.allSales.length; i++);
-
 function getThree() {
-    var random = Math.floor(Math.random() * Sales.allSales.length);
-
+    var currentPics = [];
+//choose left picture
+    currentPics[0] = getRandom();
+    while( justViewed.indexOf(currentPics[0]) !== -1) {
+        currentPics[0] = getRandom();
+    }
+//choose center picture
+    currentPics[1] = getRandom();
+    while( currentPics[0] === currentPics[1] || justViewed.indexOf(currentPics[1]) !== -1) {
+        currentPics[1] = getRandom();
+    }
+//choose right picture
+    currentPics[2] = getRandom();
+    while ( currentPics[0] === currentPics[2] || currentPics[1] === currentPics[2] || justViewed.indexOf(currentPics[2]) !== -1) {
+        currentPics[2] = getRandom();
+    }
+    justViewed = currentPics
+    display();
+    console.log(totalClicks);
 }
 
 
-//clear the indication of being shown on the previous pages pictures, indicate the new three pictures as being shown, send all three new pictures to the page, add 1 to each new picture displays values
+//function that displays the pictures to the page
+function display () {
+    first.src = sales[justViewed[0]].filepath;
+    sales[justViewed[0]].displays +=1;
 
-first.src = 
-second.src =
-third.src = 
+    second.src = sales[justViewed[1]].filepath;
+    sales[justViewed[1]].displays +=1;
+
+    third.src = sales[justViewed[2]].filepath;
+    sales[justViewed[2]].displays +=1;    
+}
 
 
-//functions that run depending on which picture was chosen
+//functions that run depending on which picture was chosen, adds a click to that picture and gets three new pictures
 function firstPic () {
-    //adds a value to the clicks for the picture chosen, then sends you to the function to pick three new random pictures
-    getThree();
+    sales[justViewed[0]].clicks +=1;
+    totalClicks +=1;
+    if(totalClicks < 25) {
+        getThree();
+    } else {
+            list();
+    }
 }
-
 function secondPic () {
-
-    getThree();
+    sales[justViewed[1]].clicks +=1;
+    totalClicks +=1;
+    if(totalClicks < 25) {
+        getThree();
+    } else {
+            list();
+    }
 }
-
 function thirdPic () {
-
-    getThree();
+    sales[justViewed[2]].clicks +=1;
+    totalClicks +=1;
+    if(totalClicks < 25) {
+        getThree();
+    } else {
+            list();
+    }
 }
 
 
 //declare an event
-populate();
+function run() {
+    populate();
+    getThree();
+}
+run();
 
 
-//an event listener for the click
-var first = document.getElementById('left');
-var second = document.getElementById('center');
-var third = document.getElementById('right');
-
+//event listeners for the click
 first.addEventListener('click', firstPic);
 second.addEventListener('click', secondPic);
 third.addEventListener('click', thirdPic)
